ef18079910960516e5bfe1427ecb2fae62dad7ab
Allen Zheng <zhenga8533@gmail.com>
2024-07-08 15:56:57 -0400

    Added champion skin toggle

---
+++ src/components/ChampionImage.tsx
@@ -0,0 +1,54 @@
+import { Image } from "@chakra-ui/react";
+import { useState } from "react";
+import { Champion } from "../hooks/useTFT";
+import { convertToPng } from "../services/format";
+
+interface ChampionImageProps {
+  champion: Champion;
+  useSkins: boolean;
+}
+
+const ChampionImage = ({ champion, useSkins }: ChampionImageProps) => {
+  const costColor: { [key: number]: string } = {
+    1: "gray",
+    2: "green",
+    3: "blue",
+    4: "purple",
+    5: "orange",
+    6: "red",
+    7: "yellow",
+  };
+
+  const getSkinImage = (tileIcon: string) => "https://raw.communitydragon.org/latest/game/" + convertToPng(tileIcon);
+  const getRawImage = (name: string) => {
+    name = name.toLowerCase().replace(/[^a-z]/g, "");
+    return `https://raw.communitydragon.org/latest/game/assets/characters/${name}/hud/${name}_square.png`;
+  };
+  const getRawFallback = (name: string) => {
+    name = name.toLowerCase().replace(/[^a-z]/g, "");
+    return `https://raw.communitydragon.org/latest/game/assets/characters/${name}/hud/${name}_square_0.png`;
+  };
+
+  const [imgSrc, setImgSrc] = useState(getRawImage(champion.name));
+  const handleError = () => {
+    if (imgSrc === getRawImage(champion.name)) {
+      setImgSrc(getRawFallback(champion.name));
+    } else {
+      setImgSrc(getSkinImage(champion.tileIcon));
+    }
+  };
+
+  return (
+    <Image
+      key={champion.apiName}
+      src={useSkins ? getSkinImage(champion.tileIcon) : imgSrc}
+      onError={handleError}
+      alt={champion.name}
+      w="100%"
+      border="1px solid black"
+      outline={`2px solid ${costColor[champion.cost]}`}
+    />
+  );
+};
+
+export default ChampionImage;
--- src/components/Champions.tsx
+++ src/components/Champions.tsx
@@ -1,21 +1,14 @@
-import { Grid, HStack, Image } from "@chakra-ui/react";
+import { Grid, HStack, Switch, Text } from "@chakra-ui/react";
+import { useState } from "react";
 import { Champion } from "../hooks/useTFT";
-import { convertToPng } from "../services/format";
+import ChampionImage from "./ChampionImage";
 
 interface ChampionsProps {
   champions: Champion[];
 }
 
 const Champions = ({ champions }: ChampionsProps) => {
-  const costColor: { [key: number]: string } = {
-    1: "gray",
-    2: "green",
-    3: "blue",
-    4: "purple",
-    5: "orange",
-    6: "red",
-    7: "yellow",
-  };
+  const [useSkins, setUseSkins] = useState(true);
 
   return (
     <>
@@ -23,18 +16,13 @@ const Champions = ({ champions }: ChampionsProps) => {
       <Grid gap={6} templateColumns="repeat(auto-fill, minmax(40px, 1fr))">
         {champions?.map(
           (champion) =>
-            champion.cost < 8 && (
-              <Image
-                key={champion.apiName}
-                src={"https://raw.communitydragon.org/latest/game/" + convertToPng(champion.tileIcon)}
-                alt={champion.name}
-                w="100%"
-                border="1px solid black"
-                outline={`2px solid ${costColor[champion.cost]}`}
-              />
-            )
+            champion.cost < 8 && <ChampionImage key={champion.apiName} champion={champion} useSkins={useSkins} />
         )}
       </Grid>
+      <HStack mt={3}>
+        <Text>Set Skins</Text>
+        <Switch isChecked={useSkins} onChange={() => setUseSkins(!useSkins)} />
+      </HStack>
     </>
   );
 };
