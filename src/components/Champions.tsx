09c18e82958e54d65490d127bdb046b722ea5da9
Allen Zheng <zhenga8533@gmail.com>
2024-07-08 16:04:37 -0400

    Added UI for champion filtering

--- src/components/Champions.tsx
+++ src/components/Champions.tsx
@@ -1,5 +1,7 @@
-import { Grid, HStack, Switch, Text } from "@chakra-ui/react";
-import { useState } from "react";
+import { Button, Grid, HStack, Input, InputGroup, InputLeftElement, Switch, Text } from "@chakra-ui/react";
+import { useRef, useState } from "react";
+import { BsSearch } from "react-icons/bs";
+import { FaCoins } from "react-icons/fa";
 import { Champion } from "../hooks/useTFT";
 import ChampionImage from "./ChampionImage";
 
@@ -8,18 +10,34 @@ interface ChampionsProps {
 }
 
 const Champions = ({ champions }: ChampionsProps) => {
+  const ref = useRef<HTMLInputElement>(null);
   const [useSkins, setUseSkins] = useState(true);
 
   return (
     <>
-      <HStack></HStack>
-      <Grid gap={6} templateColumns="repeat(auto-fill, minmax(40px, 1fr))">
+      <HStack spacing={1}>
+        <InputGroup>
+          <InputLeftElement children={<BsSearch />} />
+          <Input
+            borderRadius={1}
+            name="search"
+            placeholder="Search by name, trait, or cost..."
+            ref={ref}
+            variant="filled"
+          />
+        </InputGroup>
+        <Button borderRadius={1}>A-Z</Button>
+        <Button>
+          <FaCoins size={24} />
+        </Button>
+      </HStack>
+      <Grid gap={6} my={3} templateColumns="repeat(auto-fill, minmax(40px, 1fr))">
         {champions?.map(
           (champion) =>
             champion.cost < 8 && <ChampionImage key={champion.apiName} champion={champion} useSkins={useSkins} />
         )}
       </Grid>
-      <HStack mt={3}>
+      <HStack>
         <Text>Set Skins</Text>
         <Switch isChecked={useSkins} onChange={() => setUseSkins(!useSkins)} />
       </HStack>
