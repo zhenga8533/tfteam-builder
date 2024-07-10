4dd9762ebc33429d254f14359d6f999bda708191
Allen Zheng <zhenga8533@gmail.com>
2024-07-10 09:04:01 -0400

    Removed formatting debug

--- src/services/format.tsx
+++ src/services/format.tsx
@@ -53,8 +53,5 @@ export const parseDescription = (desc: string, values?: { [key: string]: number 
     .replace(duration, "X")
     .replace(modified, "X");
 
-  if (desc.startsWith("<spellPassive>Passive</spellPassive>: Gain <scaleHealth>@PermanentHealthPerInt"))
-    console.log(values);
-
   return parsed;
 };
